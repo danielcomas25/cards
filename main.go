a817f0f54ebfb5d314c19e409eac2da6a5b4c93e
Daniel COMAS BETANCOURT <danieljcomas@gmail.com>
2021-12-03 23:56:08 +0100

    Added write and read cards from file

--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 func main() {
 
-	cards := newDeck() // Initialized and assigned
+	// cards := newDeck() // Initialized and assigned
 
 	// hand, remainingDeck := deal(cards, 5) // Two returning values
-	cards.saveToFile("my_cards")
-
+	// cards.saveToFile("my_cards")
+	newDeckFromFile("my_cards").print()
 }
