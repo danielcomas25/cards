ca07c483cdbf52110cfb91b23c805d0be1b70aa6
Daniel COMAS BETANCOURT <danieljcomas@gmail.com>
2021-12-03 23:31:58 +0100

    Initial lessons

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+
+	cards := newDeck() // Initialized and assigned
+
+	// hand, remainingDeck := deal(cards, 5) // Two returning values
+	cards.saveToFile("my_cards")
+
+}
